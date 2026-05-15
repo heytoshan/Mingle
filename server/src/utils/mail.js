db1622c65d9b9b860a2663c4361f79288f734677
heytoshan <heyprogrammertoshan@gmail.com>
2026-05-15 11:19:16 +0530

    fixed email system

--- server/src/utils/mail.js
+++ server/src/utils/mail.js
@@ -4,17 +4,11 @@ export const sendEmail = async (options) => {
   const smtpPort = parseInt(process.env.SMTP_PORT) || 587;
   
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: smtpPort,
-    secure: smtpPort === 465, // true for port 465, false for other ports (like 587)
+    service: "gmail",
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
-    tls: {
-      // Do not fail on invalid certs
-      rejectUnauthorized: false
-    }
   });
 
   const mailOptions = {
